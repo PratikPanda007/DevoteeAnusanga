c1e429b2dea1699722ce388cbe4e8221c4536bfb
PratikPanda <pratic.panda@gmail.com>
2026-01-04 00:05:58 +0530

    Added Controller Layer

---
+++ DevoteeAnusanga/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DevoteeAnusanga.Models;
+
+namespace DevoteeAnusanga.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class HomeController : Controller
+    {
+        // GET: api/home
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(new
+            {
+                message = "Hello from HomeController (GET)",
+                serverTime = DateTime.UtcNow
+            });
+        }
+
+        // POST: api/home
+        [HttpPost]
+        public IActionResult Post([FromBody] HomeRequest request)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return Ok(new
+            {
+                message = "Data received successfully (POST)",
+                receivedData = request
+            });
+        }
+    }
+}
